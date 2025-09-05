675b1d3d6d72a6faaed0b2626a9913445ae8918c
chrisETML <christopher.ristic@yahoo.fr>
2025-09-05 11:52:58 +0200

    feat: routes

--- start/routes.ts
+++ start/routes.ts
@@ -6,11 +6,12 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
-
 import router from '@adonisjs/core/services/router'
+import StudentsController from '#controllers/students_controller'
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
+
+router.get('test', async () => {
+  return 'API is working!'
 })
+
+router.resource('students', StudentsController).apiOnly()
